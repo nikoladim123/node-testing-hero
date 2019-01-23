536c458d6b2b7364141e84ff5408ad1a88d1dd4d
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-23 21:43:09 +0100

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send('hellooo'));
+  .get('/', (req, res) => res.send('hellooo'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
