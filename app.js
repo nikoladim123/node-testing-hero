3a4fee5cf51789f7756aa97886c32eb3467f5c4d
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-23 21:46:45 +0100

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const expressip = require('express-ip');
 const PORT = process.env.PORT || 5000;
+
+express().use(expressip().getIpInfoMiddleware);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.send('hellooo'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+  express().get('/ab', function (req, res) {
+      res.send(req.ipInfo);
+  });
 
 // var response;
 // function loadDoc() {
