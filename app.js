854a261c6c8c673806b50208e1ede8ee4a267d1b
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-24 17:05:52 +0100

    cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(expressip().getIpInfoMiddleware)
   .use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", 'https://pishe.herokuapp.com/');
+      res.header("Access-Control-Allow-Origin", 'https://node-testin.herokuapp.com/api');
       res.header("Access-Control-Allow-Credentials", true);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
