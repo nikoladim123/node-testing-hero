4b93baadcde9449587c357a91e577c71a8c012fe
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-27 03:36:20 +0100

    register Form database api

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const path = require('path');
 const expressip = require('express-ip');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const PORT = process.env.PORT || 5000;
 
+const userInfoSchema = new Schema({
+  nickName:String,
+  name:String,
+  password:String,
+  email:String
+});
+
+const userInfo = mongoose.model('old user',userInfoSchema);
+mongoose.connect('mongodb://timbe:Timbetimbe91!@ds211613.mlab.com:11613/timbe');
+// mongoose.connect('mongodb://timbe:Timbetimbe91!@ds213615.mlab.com:13615/users');
+// mongoose.connect('mongodb://timbe:Timbetimbe91!@ds213615.mlab.com:13615/users');
+// mongodb://<dbuser>:<dbpassword>@ds213615.mlab.com:13615/users
 
 var ipInfo;
 express()
   .use(express.static(path.join(__dirname, 'public')))
+
   .use(expressip().getIpInfoMiddleware)
   .use(function(req, res, next) {
       res.setHeader("Access-Control-Allow-Origin", '*');
       res.setHeader("Access-Control-Allow-Credentials", true);
       res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      next();
+      next()
   })
   .get('/', (req, res) => {
     res.redirect('https://pishe.herokuapp.com/');
@@ -23,7 +41,23 @@ express()
   .get('/api',(req,res)=>{
     res.send(ipInfo);
   })
+
+  .post('/api/users',urlencodedParser,(req,res)=>{
+    var newInfo = new userInfo({
+      nickName:req.body.nickName,
+      name:req.body.name,
+      password:req.body.password,
+      email:'req.body.email'
+  });
+  newInfo.save();
+    console.log(req.body);
+    // userInfo = req.body;
+  })
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  // https://node-testin.herokuapp.com/
 
 
 
